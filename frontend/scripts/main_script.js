1364a9181f72186880a8cbefc4c3a81b4bb7b052
JaafarHawli <jaafarhawle@gmail.com>
2022-10-03 01:11:44 +0300

    frontend: added functionality to navbar elements in switching between pages

--- frontend/scripts/main_script.js
+++ frontend/scripts/main_script.js
@@ -2,6 +2,75 @@ const users = document.querySelectorAll('.user');
 const userModal = document.getElementById('userModal');
 const userQuit = document.getElementById('userQuit');
 
+// Switch between pages
+// Nav elements
+const navHome = document.getElementById('navHome');
+const navLiked = document.getElementById('navLiked');
+const navChat = document.getElementById('navChat');
+const accountButton = document.getElementById('accountButton');
+
+// Page elements
+const mainHero = document.getElementById('mainHero');
+const likedHero = document.getElementById('likedHero');
+const community = document.getElementById('community');
+const liked = document.getElementById('liked');
+const chat = document.getElementById('chat');
+const account = document.getElementById('account');
+const footer = document.getElementById('footer');
+
+navHome.addEventListener('click', () => {
+	mainHero.classList.remove('display');
+	likedHero.classList.add('display');
+	community.classList.remove('display');
+	footer.classList.remove('display');
+	liked.classList.add('display');
+	chat.classList.add('display');
+	account.classList.add('display');
+	navChat.classList.remove('current');
+	navLiked.classList.remove('current');
+	navHome.classList.add('current');
+});
+
+navLiked.addEventListener('click', () => {
+	mainHero.classList.add('display');
+	likedHero.classList.remove('display');
+	community.classList.add('display');
+	footer.classList.remove('display');
+	liked.classList.remove('display');
+	chat.classList.add('display');
+	account.classList.add('display');
+	navChat.classList.remove('current');
+	navLiked.classList.add('current');
+	navHome.classList.remove('current');
+});
+
+navChat.addEventListener('click', () => {
+	mainHero.classList.add('display');
+	likedHero.classList.remove('display');
+	community.classList.add('display');
+	footer.classList.add('display');
+	liked.classList.add('display');
+	chat.classList.remove('display');
+	account.classList.add('display');
+	navChat.classList.add('current');
+	navLiked.classList.remove('current');
+	navHome.classList.remove('current');
+});
+
+accountButton.addEventListener('click', () => {
+	mainHero.classList.add('display');
+	likedHero.classList.remove('display');
+	community.classList.add('display');
+	footer.classList.add('display');
+	liked.classList.add('display');
+	chat.classList.add('display');
+	account.classList.remove('display');
+	navChat.classList.remove('current');
+	navLiked.classList.remove('current');
+	navHome.classList.remove('current');
+});
+
+// Community section slider
 const communitySlider = tns({
 	container: '.community-grid',
 	slideBy: 1,
