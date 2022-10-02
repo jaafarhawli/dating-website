5995f4d7693c4d7479b767aadcad0c25c31a9cc2
JaafarHawli <jaafarhawle@gmail.com>
2022-10-02 19:37:41 +0300

    frontend: added liked section to main page

--- frontend/scripts/main_script.js
+++ frontend/scripts/main_script.js
@@ -1,5 +1,6 @@
 const users = document.querySelectorAll('.user');
 const userModal = document.getElementById('userModal');
+const userQuit = document.getElementById('userQuit');
 
 const communitySlider = tns({
 	container: '.community-grid',
@@ -17,4 +18,10 @@ users[0].addEventListener('click', () => {
 	userModal.showModal();
 	document.body.style.overflow = 'hidden';
 	document.body.style.userSelect = 'none';
+});
+
+userQuit.addEventListener('click', () => {
+	userModal.close();
+	document.body.style.overflow = 'auto';
+	document.body.style.userSelect = 'auto';
 });
