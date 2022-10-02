0bd168637f9aed5de955fb585cdcfbc6d059615f
JaafarHawli <jaafarhawle@gmail.com>
2022-10-02 18:05:54 +0300

    frontend: added users to user grid with a slider

---
+++ frontend/scripts/main_script.js
@@ -0,0 +1,11 @@
+const communitySlider = tns({
+	container: '.community-grid',
+	slideBy: 1,
+	speed: 400,
+	nav: false,
+	controlsContainer: '#SwipeControls',
+	prevButton: '.right',
+	nextButton: '.left',
+	gutter: 10,
+	items: 3
+});
